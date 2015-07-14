5d86909638df29fcf846323f54ccc638b81ef795
Louis Fradin <louis.fradin@gmail.com>
2015-07-14 15:52:02 +0200

    Add golang

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -47,6 +47,7 @@ apt-get install -y docker.io
 apt-get install -y zsh
 apt-get install -y python-pip
 apt-get install -y qtcreator
+apt-get install -y golang
 apt-get install -y atom
 apt-get install -y spotify-client
 apt-get install -y popcorn-time
